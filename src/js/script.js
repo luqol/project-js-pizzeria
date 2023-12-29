78f52360e1f0df699dbeb368803d69ccc8040d02
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2023-12-29 13:31:17 +0100

    Add toggle basket

--- src/js/script.js
+++ src/js/script.js
@@ -290,9 +290,36 @@
     }
   }
 
-  //class Cart{
-    
-  //}
+  class Cart{
+    constructor (element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initAction();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }   
+    initAction(){
+      const thisCart = this;
+      
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+        
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+}
 
   const app = {
     initMenu: function(){
@@ -308,6 +335,13 @@
       const thisApp = this;
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -318,6 +352,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
