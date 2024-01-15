78cad18f2a3f45836efe19b94f24be57b73cb819
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-01-15 12:19:04 +0100

    add getDate in Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {select, templates} from '../settings.js';
+import {select, templates, settings} from '../settings.js';
+import utils from '../utils.js';
 //import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
@@ -9,8 +10,66 @@ class Booking {
         const thisBooking = this;
         
         thisBooking.render(element);
-
         thisBooking.initWidgets();
+        thisBooking.getData();
+
+    }
+
+    getData(){
+        const thisBooking = this;
+
+        const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+        const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+        const params = {
+            booking: [
+                startDateParam,
+                endDateParam,
+            ],
+            eventsCurrent: [
+                settings.db.notRepeatParam,
+                startDateParam,
+                endDateParam,
+            ],
+            eventsRepeat: [
+                settings.db.repeatParam,
+                endDateParam,
+            ],
+        };
+
+        //console.log('getData params: ', paramcs);
+
+        const urls = {
+            booking:       settings.db.url + '/' + settings.db.bookings 
+                            + '?' + params.booking.join('&'),
+            eventsCurrent: settings.db.url + '/' + settings.db.events   
+                            + '?' + params.eventsCurrent.join('&'),
+            eventsRepeat:  settings.db.url + '/' + settings.db.events   
+                            + '?' + params.eventsRepeat.join("&"),
+        };
+
+        console.log('getData urls: ', urls);
+
+        Promise.all([
+            fetch(urls.booking),
+            fetch(urls.eventsCurrent),
+            fetch(urls.eventsRepeat),
+        ])
+          .then(function(allResponses){
+            const bookingsResponse = allResponses[0];
+            const eventsCurrentResponse = allResponses[1];
+            const eventsRepeatResponse = allResponses[2];
+            return Promise.all([
+                bookingsResponse.json(),
+                eventsCurrentResponse.json(),
+                eventsRepeatResponse.json(),
+            ]);
+          })
+          .then(function([bookings, eventsCurrent, eventsRepeat]){
+            console.log('rezerwacja: ', bookings);
+            console.log('eventsCurrent: ', eventsCurrent);
+            console.log('eventsRepeat: ', eventsRepeat);
+          });
 
     }
 
@@ -48,7 +107,7 @@ class Booking {
         });
 
 
-        thisBooking.dataPicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
 
         thisBooking.dom.datePicker.addEventListener('updated', function(){
 
